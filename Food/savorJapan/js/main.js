51010ce3cfa0752e6c98a9b8b8277f2a25cb6a7f
Eveyn <127455728+EvelynAkow@users.noreply.github.com>
2024-03-06 11:34:12 -0500

    updatingthe DOM

--- Food/savorJapan/js/main.js
+++ Food/savorJapan/js/main.js
@@ -1,13 +1,3 @@
-
-
-// function reply_click(clicked_id)
-//   {
-//     var food= clicked_id
-//       console.log(clicked_id);
-//   }
-
-//   document.getElementById(`${clicked_id}`).addEventListener('click',getRec)
-
 function reply_click(food){
 
 console.log(food)
@@ -15,22 +5,12 @@ console.log(food)
 .then(res => res.json())
 .then (data => {console.log(data)
 //     let i = Math.round(Math.random()*(data.drinks.length))
-// console.log(data.drinks[i])
-//  document.querySelector('h2').innerText = data.drinks[i].strDrink
-//  document.querySelector('img'). src = data.drinks[i].strDrinkThumb
+ console.log(data[0])
+  document.querySelector('h3').innerText = data[0].name
+  document.querySelector('h4').innerText = data[0].location
+  document.getElementById('foodDescript').innerText = data[0].description
+  document.getElementById('foodImg').src = data[0].image
 //  document.querySelector('a').href = data.drinks[i].strVideo
-// document.querySelector('h3').innerText = data.drinks[i].strInstructions
-// let ingredientList ='Ingredient List\n'
-// let totalCount = 15
-// for(let a = 0; a < totalCount; a++){
-//     const lookupIngredient = 'strIngredient' + (a +1)
-//     const lookupMeasue = 'strMeasure' + (a +1)
-//     const newIngredient = data.drinks[i][lookupIngredient]
-//     const newMeasure = data.drinks[i][lookupMeasue]
-//     if(newIngredient && newMeasure) ingredientList += `${newMeasure} of ${newIngredient}\n`
-// }
-// console.log({ingredientList})
-// document.querySelector('p').innerText = ingredientList
 })
 .catch(err => {
     console.log(`error ${err}`)
